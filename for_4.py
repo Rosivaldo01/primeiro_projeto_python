10fbe27ad2b51aa8326382f9358fb75218d17131
Denize Sanches da Paixão <rosivaldo.correia@terra.com.br>
2022-05-15 21:56:15 -0300

    aula 4 atualizada

---
+++ for_4.py
@@ -0,0 +1,23 @@
+from random import randint
+for i in range(1, 10):
+    # neste caso não será informado o else.
+    if i == 6:
+        break
+    print(i)
+else:
+    print('fim!')
+
+
+def sortear_dado():
+    return randint(1, 6)
+
+
+for i in range(1, 7):
+    if i % 2 == 1:
+        continue
+
+    if sortear_dado() == i:
+        print('Acertou ', i)
+        break
+    else:
+        print('Não acertou o número')
